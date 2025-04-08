fcf5ac443318f5f8cff7a8812090076691af8586
modpanyakorn <panyakorn.mod@gmail.com>
2025-04-09 04:10:32 +0700

    del

--- src/backend/modules/admin/admin.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("./admin.controller");
-
-// router.post("/bookRoom", controller.bookRoom);
-router.get("/getSchedule", controller.getSchedule);
-// เพิ่มได้เรื่อย ๆ
-
-module.exports = router;
